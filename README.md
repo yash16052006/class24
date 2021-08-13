c35677c030010af6ed47763443680545e22b09d1
yash16052006 <86788569+yash16052006@users.noreply.github.com>
2021-08-13 17:50:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# class24
+# PiratesInvasionStage-1.5
+created rotating cannon
---
+++ js/cannonBall.js
@@ -0,0 +1,42 @@
+class CannonBall{
+    constructor(x, y){
+        var options  = {
+            restitution:0.8, 
+            friction:1.0,
+            density:1.0, 
+            isStatic:true
+        }
+    this.body = Bodies.circle(x, y, 40, options);
+    this.image = loadImage("./assets/cannonball.png");
+    World.add(world, this.body)
+    }
+
+   shoot(){
+       var velocity = p5.Vector.fromAngle(cannon.angle);
+       velocity.mult(20);
+       Matter.Body.setStatic(this.body, false);
+       Matter.Body.setVelocity(this.body, {
+           x:velocity.x, 
+           y:velocity.y
+       })
+   }
+
+    display(){
+    var pos = this.body.position
+    var angle = this.body.angle
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, 40, 40);
+    // fill("black");
+    //stroke("blue");
+    //strokeWeight(7);
+    //ellipseMode(RADIUS)
+    //ellipse(0, 0, 25, 25)
+
+    
+    pop();
+    }
+}
